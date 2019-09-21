bfb7798d9a839417d9d7bfb69462791d3db0a63e
jakazzy <jakazzy@gmail.com>
2019-09-21 00:31:55 +0000

    add card to the list

---
+++ src/components/card/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Card = props => {
+  return <div></div>;
+};
+
+Card.propTypes = {
+  name: PropTypes.string
+};
+
+export default Card;
---
+++ src/data/Data.js
@@ -0,0 +1,84 @@
+const data = [
+  {
+    id: 1,
+    name: "Vue",
+    img: "vuejsimage"
+  },
+  {
+    id: 2,
+    name: "Vue",
+    img: "Vuejsimage"
+  },
+  {
+    id: 3,
+    name: "React",
+    img: "Reactjsimage"
+  },
+  {
+    id: 4,
+    name: "React",
+    img: "Reactjsimage"
+  },
+  {
+    id: 5,
+    name: "Angular",
+    img: "Angularjsimage"
+  },
+  {
+    id: 6,
+    name: "Angular",
+    img: "Angularjsimage"
+  },
+  {
+    id: 7,
+    name: "meteor",
+    img: "Meteorjsimage"
+  },
+  {
+    id: 8,
+    name: "meteor",
+    img: "Meteorjsimage"
+  },
+  {
+    id: 9,
+    name: "Ember",
+    img: "Emberjsimage"
+  },
+  {
+    id: 10,
+    name: "Ember",
+    img: "Emberjsimage"
+  },
+  {
+    id: 11,
+    name: "Svelte",
+    img: "Sveltejsimage"
+  },
+  {
+    id: 12,
+    name: "Svelte",
+    img: "Sveltejsimage"
+  },
+  {
+    id: 13,
+    name: "Electron",
+    img: "Electronjsimage"
+  },
+  {
+    id: 14,
+    name: "Electron",
+    img: "Electronjsimage"
+  },
+  {
+    id: 15,
+    name: "Reactnative",
+    img: "Reactnativejsimage"
+  },
+  {
+    id: 16,
+    name: "Reactnative",
+    img: "Reactnativejsimage"
+  }
+];
+
+export default data;
