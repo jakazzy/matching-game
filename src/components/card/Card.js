8546a087a99c91dad799fee64f0b66a5048c0a4c
jakazzy <jakazzy@gmail.com>
2019-09-21 00:46:15 +0000

    add card to list

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./card.css";
 
 const Card = props => {
-  return <div></div>;
+  return (
+    <li className="card">
+      <div id="card1">
+        <p>this is a card</p>
+      </div>
+    </li>
+  );
 };
 
 Card.propTypes = {
---
+++ src/components/cardList/CardList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./../card/Card";
+
+function CardList() {
+  return (
+    <ul class="container">
+      <Card />
+    </ul>
+  );
+}
+
+export default CardList;
