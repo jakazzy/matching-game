a50b623147e437565ae0af217f874932e140d0c0
jakazzy <jakazzy@gmail.com>
2019-09-21 17:52:09 +0000

    card rotates when clicked

--- src/contexts/CardContext.js
+++ src/contexts/CardContext.js
@@ -5,10 +5,14 @@ export const CardContext = createContext();
 function CardContextProvider(props) {
   // eslint-disable-next-line no-unused-vars
   const [cards, setCard] = useState(data);
+
   const handleCardClick = id => {
-    let card = cards.filter(card => card.id === id);
-    console.log(card);
-    // setCard({ ...values, [event.target.id]: event.target.value });
+    let cardlist = cards.map(card => {
+      return id === card.id ? Object.assign(card, { flipped: true }) : card;
+    });
+    console.log(cardlist);
+
+    setCard(cardlist);
   };
   return (
     <CardContext.Provider value={{ cards, handleCardClick }}>
