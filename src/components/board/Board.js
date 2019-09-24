b7b78219becda4bf2ae88ac7dca7775d365cda4e
jakazzy <jakazzy@gmail.com>
2019-09-24 22:34:42 +0000

    add new game button ui

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CardList from "./../cardList/CardList";
 import CardContextProvider from "../../contexts/CardContext";
+import Button from "../button/Button";
 import "./board.css";
 
 const Board = () => {
@@ -9,6 +10,7 @@ const Board = () => {
       <div id="title">
         <h1>card game</h1>
         <h2>Let's have fun! Match all the cards with their pairs.</h2>
+        <Button />
       </div>
       <CardContextProvider>
         <CardList />
---
+++ src/components/button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./button.css";
+
+function Button() {
+  return (
+    <div>
+      <button className="button">Start</button>
+    </div>
+  );
+}
+
+export default Button;
